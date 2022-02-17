470033d6eaf7d6e9655624a2581efe7ec854d1fa
M.Battista <info@graficatek.it>
2022-02-17 10:26:59 +0100

    organizzo file

--- SKIN/secondasoluzione/exit.js
+++ SKIN/secondasoluzione/exit.js
@@ -1,16 +1,31 @@
 //variabili
 var exitUrl = "https://www.google.com";
 
+
 //carico il DOM
 window.addEventListener('load', function(){
-    console.warn('inizializzo la skin');
+    
+    console.log("inizializzo la skin");
     var body = document.getElementsByTagName('body')[0];
 
-    let a = document.createElement("a");
-    console.warn(a);
-    var t = this.document.createTextNode("ClickTag Skin");
-    a.appendChild(t);
-    document.body.appendChild(a);
+    //creo il tag <a>
+    let ancora = document.createElement("a");
+    console.log(ancora);
+
+
+    let span = document.createElement("span");
+    
+     var t = this.document.createTextNode("ClickTag Skin");
+     span.appendChild(t);
+     ancora.appendChild(span);
+   
+     ancora.setAttribute("id", "skin-link");
+   
+   
+   
+   
+   
+     // document.body.appendChild(a);
     
     //console.info('aggiungo il link al body');
     //body.addEventListener('click', function(){
@@ -22,3 +37,8 @@ window.addEventListener('load', function(){
 });
 
    
+/*  
+    <a id="skin-link" href="https://www.google.com" target="_blank">
+        <span>ClickTag Skin</span>
+    </a>
+*/
