202d72f81e160cf6f53f2f972dc887acfb365856
M.Battista <info@graficatek.it>
2022-02-17 01:10:19 +0100

    create Html with JS

--- SKIN/exit.js
+++ SKIN/exit.js
@@ -1,8 +1,24 @@
-var body = document.getElementsByTagName('body')[0];
-let exitUrl = "https://www.google.com";
-console.log(body);
+//variabili
+var exitUrl = "https://www.google.com";
 
-body.addEventListener('click', function(){
-    console.log(window.location.href);
-    //window.location.href = exitUrl;
-})
+//carico il DOM
+window.addEventListener('load', function(){
+    console.warn('inizializzo la skin');
+    var body = document.getElementsByTagName('body')[0];
+
+    let a = document.createElement("a");
+    console.warn(a);
+    var t = this.document.createTextNode("ClickTag Skin");
+    a.appendChild(t);
+    document.body.appendChild(a);
+    
+    //console.info('aggiungo il link al body');
+    //body.addEventListener('click', function(){
+
+        // console.warn(window.location.href);
+        // window.location.href = exitUrl;
+    //});
+
+});
+
+   
